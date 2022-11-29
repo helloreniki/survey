4861fb51a27da95384cc3c341504fcf4ebda2126
Reniki <hello@reniki.com>
2022-11-29 15:35:49 +0100

    post data to firebase db, get data from db

--- src/Components/Survey/SurveyQuestions.vue
+++ src/Components/Survey/SurveyQuestions.vue
@@ -1,27 +1,65 @@
 <template>
     <BaseCard>
         <h1 class="text-xl font-semibold mb-6">How was your learning experience?</h1>
-        <form action="" class="flex flex-col gap-8">
+        <form @submit.prevent="submitSurvey" class="flex flex-col gap-8">
+            <p v-if="invalidInput" class="text-sm text-red-500">Please fill in all fields.</p>
             <div>
                 <label for="name">Your name:</label>
-                <input type="text" id="name" name="name" class="border ml-2">
+                <input type="text" id="name" name="name" class="border ml-2" v-model.trim="enteredName">
             </div>
             <div class="flex flex-col gap-2">
                 <h3 class="text-lg">Please rate your learning experience...</h3>
                 <div class="flex gap-2 items-center">
-                    <input type="radio" id="rating" name="rating" value="poor">
+                    <input type="radio" id="rating" name="rating" value="poor" v-model="chosenRating">
                     <label for="rating">Poor</label>
                 </div>
                 <div class="flex gap-2 items-center">
-                    <input type="radio" id="rating" name="rating" value="average">
+                    <input type="radio" id="rating" name="rating" value="average" v-model="chosenRating">
                     <label for="rating">Average</label>
                 </div>
                 <div class="flex gap-2 items-center">
-                    <input type="radio" id="rating" name="rating" value="great">
+                    <input type="radio" id="rating" name="rating" value="great" v-model="chosenRating">
                     <label for="rating">Great</label>
                 </div>
             </div>
-            <BaseButton class="self-start">Submit</BaseButton>
+            <BaseButton type="submit" class="self-start">Submit</BaseButton>
+            <p v-if="error">{{ error }}</p>
         </form>
     </BaseCard>
+    {{ enteredName }}
+    {{ chosenRating }}
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const enteredName = ref('');
+const chosenRating = ref('');
+const invalidInput = ref(false);
+
+function submitSurvey() {
+    if(enteredName.value === '' || chosenRating.value === null) {
+        invalidInput.value = true;
+        return;
+    }
+
+    invalidInput.value = false;
+    // console.log(enteredName.value),
+    // console.log(chosenRating.value),
+    console.log('method submitSurvey'),
+    fetch('https://survey-93121-default-rtdb.europe-west1.firebasedatabase.app/survey.json', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: enteredName.value, // .value because of ref('')
+            rating: chosenRating.value
+        })
+    })
+
+    enteredName.value = '';
+    chosenRating.value = null;
+}
+
+</script>
--- src/Components/Survey/UserExperiences.vue
+++ src/Components/Survey/UserExperiences.vue
@@ -1,5 +1,50 @@
 <template>
     <BaseCard>
-        <h1>User Experiences</h1>
+        <h1 class="text-xl font-semibold mb-6">User Experiences</h1>
+        <BaseButton @click="loadExperiences">Load User Experiences</BaseButton>
+        <p v-if="isLoading">Loading...</p>
+        <!-- {{ results.length }}
+        {{ isLoading }} -->
+        <p v-if="!isLoading && (!results || results.length === 0)">Please add some data...</p>
+        <p v-if="!isLoading && results && results.length > 0" v-for="result in results" :key="result.id" class="my-4"><span class="font-semibold">{{ result.name}}</span> rated experience <span class="text-violet-700 font-semibold">{{ result.rating }}</span>.</p>
     </BaseCard>
 </template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const results = ref([]);
+const isLoading = ref(false);
+
+
+function loadExperiences() {
+    // console.log('loadexp');
+    isLoading.value = true;
+    fetch('https://survey-93121-default-rtdb.europe-west1.firebasedatabase.app/survey.json')
+    .then(response => {
+        if(response.ok){
+            // console.log(response);
+            return response.json();
+        }
+    })
+    .then(data => {
+        // console.log(data)
+        isLoading.value = false;
+        const x = [];
+        for(const id in data) {
+            // console.log(id.name)
+            x.push({
+                id: id,
+                name: data[id].name,
+                rating: data[id].rating
+            });
+        }
+        // console.log(x)
+        results.value = x;
+    })
+
+}
+
+onMounted(() => loadExperiences());
+
+</script>
