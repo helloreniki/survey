21cce712d9c7bf436e807c09481608d68a3bbc95
Reniki <hello@reniki.com>
2022-12-02 17:02:45 +0100

    npm install pinia, create pinia instance in main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { createApp } from "vue";
+import { createPinia } from 'pinia';
 import mitt from 'mitt'
 import App from "./App.vue";
 import BaseCard from "./Components/UI/BaseCard.vue";
 import BaseButton from "./Components/UI/BaseButton.vue";
 
+const pinia = createPinia()
 const emitter = mitt()
+
+const app = createApp(App)
 
 // import "./assets/main.css";
 
-createApp(App)
-    .component('BaseCard', BaseCard)
+ app.component('BaseCard', BaseCard)
     .component('BaseButton', BaseButton)
     .provide('emitter', emitter)
+    .use(pinia)
     .mount("#app");
